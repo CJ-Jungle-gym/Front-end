cc98c5b1519710b496541b22702301df91f20415
MASEOKJAE <maasj7514@gmail.com>
2025-02-16 19:48:36 +0900

    feat: Login 페이지 구현 완료

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,47 @@
 import React from "react";
-// import "../assets/login-bg.jpg"; // 배경 이미지
+import "../styles/Login.css";
+import cjOneLogo from "../assets/cjone-logo.png";
+import kakaoIcon from "../assets/kakao-icon.png";
 
 function Login() {
   return (
-    <div
-      style={{
-        backgroundImage: "url(/assets/login-bg.jpg)",
-        backgroundSize: "cover",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
-      }}
-    >
-      <div style={{ backgroundColor: "white", padding: "20px", borderRadius: "8px" }}>
-        <h2>로그인</h2>
-        <input type="text" placeholder="아이디" />
-        <input type="password" placeholder="비밀번호" />
-        <button>로그인</button>
+    <div className="login-container">
+      <div className="login-box">
+        <h2 className="login-title">로그인</h2>
+
+        {/* 로그인 폼 */}
+        <form className="login-form">
+          <input type="text" placeholder="CJ ONE 통합회원 아이디" className="login-input" />
+          <input type="password" placeholder="비밀번호 (8~12자, 영문+숫자+특수문자)" className="login-input" />
+
+          <div className="login-options">
+            <label className="checkbox-container">
+              <input type="checkbox" />
+              <span className="checkbox-text">아이디 저장</span>
+            </label>
+            <div className="find-links">
+              <a href="#">아이디 찾기</a>
+              <span>|</span>
+              <a href="#">비밀번호 찾기</a>
+            </div>
+          </div>
+
+          <button type="submit" className="login-button">로그인</button>
+          <button type="button" className="kakao-button">
+            <img src={kakaoIcon} alt="카카오 로그인" className="kakao-icon" />
+            카카오로 로그인
+          </button>
+        </form>
+
+        {/* CJ ONE 회원가입 안내 */}
+        <div className="cj-one-section">
+          <img src={cjOneLogo} alt="CJ ONE" className="cj-one-logo" />
+          <p className="cj-one-text">
+            CJ ONE 통합회원으로 가입하고<br />
+            올리브영에서 편안한 쇼핑하세요
+          </p>
+          <button className="signup-button">회원가입</button>
+        </div>
       </div>
     </div>
   );
